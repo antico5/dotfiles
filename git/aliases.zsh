1964daf9ed70f2de881a153689fc4801d21c9088
antico5 <armando.andini@hotmail.com>
2015-02-13 12:21:55 -0300

    Add alias.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,10 +10,11 @@ fi
 alias gl='git log'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gd='git diff'
-alias gp='git pull'
+alias gp='git pull --rebase'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gpu='git push'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias v='vim'
 alias ls='ls -la'
+alias be='bundle exec'
